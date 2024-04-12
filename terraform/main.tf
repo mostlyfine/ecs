bf1d9da72126694967131d2f766f3cbb070c0b64
mostlyfine <mostlyfine@gmail.com>
2024-04-12 16:41:17 +0900

    upgrade.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "1.7.4"
+  required_version = "1.8.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.36.0"
+      version = "5.45.0"
     }
   }
   backend "s3" {
